42a8eecf86bfe757f7061dd50bdf524fbc39eaa1
cola314 <ubj0314@naver.com>
2022-06-09 22:33:25 +0900

    fix: 편집기 영역에서 탭키 입력이 동작 안함 #2
    TextBox AcceptsTab 옵션 true로 설정
    
    Resolved: #2

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -147,6 +147,7 @@ namespace SimpleMarkdown
             // 
             // textBox
             // 
+            this.textBox.AcceptsTab = true;
             this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox.Location = new System.Drawing.Point(0, 0);
